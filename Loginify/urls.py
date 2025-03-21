2a7335423c4450151098707a50b1cf48fcca3d07
yashatre98 <yashatre98@gmail.com>
2025-03-21 05:36:02 -0700

    Task-4, Task-5, and readme updated.

--- Loginify/urls.py
+++ Loginify/urls.py
@@ -5,5 +5,9 @@ urlpatterns = [
     path('task-2/', views.hello_world, name='hello'),
     path('signup/', views.signup, name='signup'),
     path('login/', views.login, name='login'),
+    path('users/', views.get_all_users, name='get_all_users'),
+    path('users/<str:email>/', views.get_user_by_email, name='get_user_by_email'),
+    path('users/update/<str:email>/', views.update_user, name='update_user'),
+    path('users/delete/<str:email>/', views.delete_user, name='delete_user'),
 
 ]
--- Loginify/views.py
+++ Loginify/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.views.decorators.csrf import csrf_exempt
+import json
 from .models import UserDetails
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+
+
 
 # Task-2 hello view
 def hello_world(request):
@@ -46,3 +50,43 @@ def login(request):
             return redirect('login')
 
     return render(request, 'login.html')
+
+# Crud operations, Task-5
+def get_all_users(request):
+    users = list(UserDetails.objects.values())  # Convert QuerySet to list
+    return JsonResponse(users, safe=False)
+
+def get_user_by_email(request, email):
+    try:
+        user = UserDetails.objects.get(email=email)
+        return JsonResponse({"username": user.username, "email": user.email})
+    except UserDetails.DoesNotExist:
+        return JsonResponse({"error": "User not found"}, status=404)
+
+
+@csrf_exempt
+def update_user(request, email):
+    if request.method == "PUT":
+        try:
+            user = UserDetails.objects.get(email=email)
+            print("$$$$$", user.username, "$$$$$")
+            data = json.loads(request.body)
+            print("#####",data.get("username"),"#####", "#####")
+            # user.username = data.get("username")
+            user.password = data.get("password")
+
+            user.save(update_fields=['password'])
+
+            return JsonResponse({"message": "User updated successfully"})
+        except UserDetails.DoesNotExist:
+            return JsonResponse({"error": "User not found"}, status=404)
+
+@csrf_exempt
+def delete_user(request, email):
+    if request.method == "DELETE":
+        try:
+            user = UserDetails.objects.get(email=email)
+            user.delete()
+            return JsonResponse({"message": "User deleted successfully"})
+        except UserDetails.DoesNotExist:
+            return JsonResponse({"error": "User not found"}, status=404)
