3a2f339780cd4d9554eaf433c4f62997f3b74a56
yashatre98 <yashatre98@gmail.com>
2025-03-21 03:39:49 -0700

    Task-1, and Task-2 is done

---
+++ Loginify/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class LoginifyConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Loginify'
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # DjangoProject
+Task-1 Screen shots:
+
+![img.png](img.png)
+
+![img_1.png](img_1.png)
+
+![img_2.png](img_2.png)
